40cec229c622145a7f4d7e777e19646ff4d12439
kongyajie <kongyajie@bilibili.com>
2025-10-26 18:28:03 +0800

    feat: add svelte-todolist

---
+++ svelte-todolist/src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import TodoList from './lib/TodoList.svelte';
+</script>
+
+<main>
+  <div class="card">
+    <!-- <Counter /> -->
+    <TodoList />
+  </div>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ svelte-todolist/src/lib/TodoList.svelte
@@ -0,0 +1,16 @@
+<script>
+  const title = $state('TodoList');
+  const todoList = $state([
+    {
+      id: 1,
+      name: 'aaaaa'
+    }
+  ])
+</script>
+
+
+<h4>{title}</h4>
+
+<ul>
+  <li>aaa</li>
+</ul>
